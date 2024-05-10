30ec3fd0bbd4b9bc38f2ee251c8f835d6b697584
Faqih <faqihuddn17@gmail.com>
2024-05-10 21:51:55 +0700

    puskesmas-app

---
+++ dbpuskesmas.sql
@@ -0,0 +1,249 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 10, 2024 at 04:44 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbpuskesmas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelurahan`
+--
+
+CREATE TABLE `kelurahan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `kec_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kelurahan`
+--
+
+INSERT INTO `kelurahan` (`id`, `nama`, `kec_id`) VALUES
+(1, 'Leuwimekarasdasasdas', 2),
+(3, 'asdas', 21);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paramedik`
+--
+
+CREATE TABLE `paramedik` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `tmp_lahir` varchar(30) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `kategori` enum('Dokter GIGI','Bidan') NOT NULL,
+  `telepon` varchar(20) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `unit_kerja_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `paramedik`
+--
+
+INSERT INTO `paramedik` (`id`, `nama`, `gender`, `tmp_lahir`, `tgl_lahir`, `kategori`, `telepon`, `alamat`, `unit_kerja_id`) VALUES
+(1, 'ibahi', '1', 'mafioqwq0', '2022-09-28', 'Dokter GIGI', '19910388', 'edjojpa', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id` int(11) NOT NULL,
+  `kode` varchar(10) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `tmp_lahir` varchar(30) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `gender` char(1) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `kelurahan_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id`, `kode`, `nama`, `tmp_lahir`, `tgl_lahir`, `gender`, `email`, `alamat`, `kelurahan_id`) VALUES
+(3, 'A108', 'Nagita nafisa', 'Jogja', '1993-03-08', '0', 'nagtnfsaa@gmail.com', 'Jogja, Jawa tengah', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `periksa`
+--
+
+CREATE TABLE `periksa` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `berat` double NOT NULL,
+  `tinggi` double NOT NULL,
+  `tensi` varchar(20) NOT NULL,
+  `keterangan` varchar(100) NOT NULL,
+  `pasien_id` int(11) NOT NULL,
+  `dokter_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `periksa`
+--
+
+INSERT INTO `periksa` (`id`, `tanggal`, `berat`, `tinggi`, `tensi`, `keterangan`, `pasien_id`, `dokter_id`) VALUES
+(0, '2008-02-12', 77, 178, '67', 'sehat', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit_kerja`
+--
+
+CREATE TABLE `unit_kerja` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `unit_kerja`
+--
+
+INSERT INTO `unit_kerja` (`id`, `nama`) VALUES
+(1, 'ibahi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) DEFAULT NULL,
+  `email` varchar(105) NOT NULL,
+  `password` varchar(145) NOT NULL,
+  `role` enum('admin','operator') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `email`, `password`, `role`) VALUES
+(1, 'faqihuddin', 'faqihuddn17@gmail.com', '202cb962ac59075b964b07152d234b70', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kelurahan`
+--
+ALTER TABLE `kelurahan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paramedik`
+--
+ALTER TABLE `paramedik`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_unit_kerja` (`unit_kerja_id`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kelurahan_id` (`kelurahan_id`);
+
+--
+-- Indexes for table `periksa`
+--
+ALTER TABLE `periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_pasien_id` (`pasien_id`),
+  ADD KEY `fk_dokter_id` (`dokter_id`);
+
+--
+-- Indexes for table `unit_kerja`
+--
+ALTER TABLE `unit_kerja`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kelurahan`
+--
+ALTER TABLE `kelurahan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `paramedik`
+--
+ALTER TABLE `paramedik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `unit_kerja`
+--
+ALTER TABLE `unit_kerja`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `paramedik`
+--
+ALTER TABLE `paramedik`
+  ADD CONSTRAINT `fk_unit_kerja` FOREIGN KEY (`unit_kerja_id`) REFERENCES `unit_kerja` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD CONSTRAINT `pasien_ibfk_1` FOREIGN KEY (`kelurahan_id`) REFERENCES `kelurahan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `periksa`
+--
+ALTER TABLE `periksa`
+  ADD CONSTRAINT `fk_dokter_id` FOREIGN KEY (`dokter_id`) REFERENCES `paramedik` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_pasien_id` FOREIGN KEY (`pasien_id`) REFERENCES `pasien` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
